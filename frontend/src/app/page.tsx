e56045fd041ca2488fd9d55bff44164e7d64186f
abdelrhman ebian <abdelrhmanebian@gmail.com>
2024-07-27 20:44:28 +0300

    ssr = false

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,14 +3,13 @@ import Featured from "../components/Featured";
 import CategoryList from "../components/CategoryList";
 import { GET_CATEGORIES, getAllPosts, getPostOnly } from "../schema/query";
 import ApolloWrapper from "../components/ApolloClient";
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import dynamic from "next/dynamic";
 import Loading from "../components/Loading";
 import { client } from "../components/apolloInitialize";
 const DynamicCardList = dynamic(() => import("../components/CardList"), {
-  ssr: true,
+  ssr: false,
 });
-const DynamicMenu = dynamic(() => import("../components/Menu"), { ssr: true });
+const DynamicMenu = dynamic(() => import("../components/Menu"), { ssr: false });
 const Page = async () => {
   const { props } = await getData();
   return (
@@ -80,7 +79,6 @@ export async function getData() {
       },
     };
   } catch (error) {
-    console.log(error)
     return {
       post: null,
       categories: [],
