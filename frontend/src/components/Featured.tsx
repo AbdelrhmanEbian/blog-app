60d03cc183cdacd53b1726db1e471c546a7f6a23
abdelrhman ebian <abdelrhmanebian@gmail.com>
2024-07-28 13:21:13 +0300

    featured

--- frontend/src/components/Featured.tsx
+++ frontend/src/components/Featured.tsx
@@ -1,42 +1,50 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import { post } from '../schema/type';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { post } from "../schema/type";
 const Featured = ({ post }: { post: post | null }) => {
   return (
     <div>
-      <h1 className='text-3xl md:text-5xl font-[500]'>
-        <span className='font-bold'> Hey, There! </span> Discover new stories and creative ideas.
+      <h1 className="text-3xl md:text-4xl font-[500]">
+        <span className="font-bold text-4xl md:text-5xl">Hey , There!</span> Discover new stories
+        and creative ideas.
       </h1>
-      <div className='text-[60px] min-h-[300px] md:flex my-[70px] items-center gap-8'>
-        <div className="h-full   w-1/2 flex-col items-center justify-between  max-md:w-full flex-1 relative">
-            <Image
-          sizes="(max-width: 600px) 100vw, 
+      <div className="text-[60px]my-7  max-md:border-b-2 pb-1 border-secondary-focus flex max-md:gap-2 max-md:flex-col gap-7 md:h-[350px] items-center rounded-lg ">
+        <div className="w-full md:w-1/2 h-[350px] relative">
+          <Image
+            sizes="(max-width: 600px) 100vw, 
           (max-width: 1200px) 50vw, 
           33vw"
-          loading="lazy"
-          className="rounded-lg"
-          width={200}
-          height={250}
-          style={{
-            objectFit: "contain",
-            width: "fit-content",
-            margin: "auto",
-            height: "100%",
-          }}
-          alt={post?.img ? post?.title : "Default post image"}
-          src={post?.img ? post?.img : "/p1.jpeg"}
-        />        </div>
-        <div className='gap-5 max-w-full flex max-md:mt-[20px] md:min-h-[300px] justify-evenly flex-col flex-1'>
-          <div className='py-1'>
-            <h2 className='md:text-[30px] break-words text-3xl font-semibold'>
+            loading="lazy"
+            className="rounded-lg"
+            width={200}
+            height={250}
+            style={{
+              objectFit: "contain",
+              width: "fit-content",
+              margin: "auto",
+              height: "100%",
+            }}
+            alt={post?.img ? post?.title : "Default post image"}
+            src={post?.img ? post?.img : "/p1.jpeg"}
+          />
+        </div>
+        <div className="gap-5 max-w-full flex max-md:mt-[20px] md:min-h-[300px] justify-evenly flex-col flex-1">
+          <div className="py-1">
+            <h2 className="md:text-[30px] break-words text-3xl font-semibold">
               {post?.title}
             </h2>
           </div>
-          <p className='text-xl break-words md:text-[20px] font-light text-accent'>
+          <p className="text-xl break-words md:text-[20px] font-light text-accent">
             {post?.desc}
           </p>
-            <Link className=' btn-neutral w-max btn ' aria-label={"our popular post is about" + post?.title} href={`blog/${post?.id}`}>Explore Post</Link>
+          <Link
+            className=" btn-neutral w-max btn "
+            aria-label={"our popular post is about" + post?.title}
+            href={`blog/${post?.id}`}
+          >
+            Explore Post
+          </Link>
         </div>
       </div>
     </div>
